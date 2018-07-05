73624a7aa5ffd2ea91b5d13c1a194fc09e43c3b0
Mengjiao0714 <30472909+Mengjiao0714@users.noreply.github.com>
2018-07-05 09:32:53 -0500

    Update va_lag.R

--- R/va_lag.R
+++ R/va_lag.R
@@ -12,20 +12,29 @@
 #' @import dplyr
 #' @import tidyr
 #' @export
-va_lag <- function(data, names, offset) {
-  return(
-    data %>% # take raw data
-      select(C_BioSense_ID, C_Biosense_Facility_ID, Arrived_Date_Time, C_Visit_Date_Time) %>% # select only vars we need
-      group_by(C_BioSense_ID) %>% # group by patient visit
-      slice(which.min(Arrived_Date_Time)) %>% # get the first arrived date time
-      slice(1) %>% # get one, just in case there are ties
-      mutate(
-        Arrived_Date_Time = lubridate::ymd_hms(Arrived_Date_Time) - lubridate::hours(offset), # adjust utc arrived to local time
-        lag=as.numeric(difftime(Arrived_Date_Time, C_Visit_Date_Time, units="hours")) # get lag, in hours
-      ) %>% 
-      ungroup() %>% # ungroup explicitly
-      group_by(C_Biosense_Facility_ID) %>% # regroup by facility
-      summarise(First_Message=round(mean(lag, na.rm=TRUE),2)) %>% # get the average, round by two digits
-      right_join(names, ., by="C_Biosense_Facility_ID") # join with the names data
-  )
+va_lag <- function(data) {
+    LagTime=data %>% 
+      select(C_Biosense_Facility_ID, Arrived_Date_Time, C_Visit_Date_Time, Message_Date_Time, Recorded_Date_Time)%>% 
+      mutate(Arrived=as.POSIXct(Arrived_Date_Time,format="%Y-%m-%d %H:%M:%S"),
+             Visit=as.POSIXct(C_Visit_Date_Time,format="%Y-%m-%d %H:%M:%S"),
+             Message=as.POSIXct(Message_Date_Time,format="%Y-%m-%d %H:%M:%S"),
+             Record=as.POSIXct(Recorded_Date_Time,format="%Y-%m-%d %H:%M:%S")
+      )
+    
+    Time_Diff=LagTime%>%
+      transmute(C_Biosense_Facility_ID,
+                lag_Arrival_Visit=as.numeric(difftime(Arrived,Visit,units="hours")),
+                lag_Arrival_Message=as.numeric(difftime(Arrived,Message,units="hours")),
+                lag_Message_Record=as.numeric(difftime(Message,Record,units="hours")),
+                lag_Record_Visit=as.numeric(difftime(Record,Visit,units="hours"))
+      )
+    
+   return(
+     Lag_Summary=Time_Diff %>%
+       group_by(C_Biosense_Facility_ID)%>%
+       summarise(Arrival_Visit=round(mean(lag_Arrival_Visit,na.rm=TRUE),2),
+                 Arrival_Message=round(mean(lag_Arrival_Message,na.rm=TRUE),2),
+                 Message_Record=round(mean(lag_Message_Record,na.rm=TRUE),2),
+                 Record_Visit=round(mean(lag_Record_Visit,na.rm=TRUE),2))
+   )
 }
