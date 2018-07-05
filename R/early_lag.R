42a2198a231103e64f09bcf53371e1fdc2a1be68
Mengjiao0714 <30472909+Mengjiao0714@users.noreply.github.com>
2018-07-05 12:58:51 -0500

    Update early_lag.R

--- R/early_lag.R
+++ R/early_lag.R
@@ -14,19 +14,19 @@ early_lag <- function(data) {
   
   
   Time_Diff=Early_Lag%>%
-    mutate(lag_Arrival_Visit=as.numeric(difftime(Arrived,Visit,units="hours")),
-           lag_Arrival_Message=as.numeric(difftime(Arrived,Message,units="hours")),
+    mutate(lag_Record_Visit=as.numeric(difftime(Record,Visit,units="hours")),
            lag_Message_Record=as.numeric(difftime(Message,Record,units="hours")),
-           lag_Record_Visit=as.numeric(difftime(Record,Visit,units="hours"))
+           lag_Arrival_Message=as.numeric(difftime(Arrived,Message,units="hours")),
+           lag_Arrival_Visit=as.numeric(difftime(Arrived,Visit,units="hours"))         
     )
   
   return(
   Lag_Summary=Time_Diff %>%
     group_by(C_Biosense_Facility_ID)%>%
-    summarise(Arrival_Visit=round(mean(lag_Arrival_Visit,na.rm=TRUE),2),
-              Arrival_Message=round(mean(lag_Arrival_Message,na.rm=TRUE),2),
+    summarise(Record_Visit=round(mean(lag_Record_Visit,na.rm=TRUE),2)),
               Message_Record=round(mean(lag_Message_Record,na.rm=TRUE),2),
-              Record_Visit=round(mean(lag_Record_Visit,na.rm=TRUE),2))
+              Arrival_Message=round(mean(lag_Arrival_Message,na.rm=TRUE),2),
+              Arrival_Visit=round(mean(lag_Arrival_Visit,na.rm=TRUE),2)         
   )
 
 }
