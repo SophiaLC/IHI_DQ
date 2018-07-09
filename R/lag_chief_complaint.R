d954c7d5cca7577515993ec2b0d739128368915c
Mengjiao0714 <30472909+Mengjiao0714@users.noreply.github.com>
2018-07-09 09:25:44 -0500

    Update lag_chief_complaint.R

--- R/lag_chief_complaint.R
+++ R/lag_chief_complaint.R
@@ -8,13 +8,14 @@ lag_chief_complaint<-function(data){
     filter(is.na(C_Chief_Complaint)==FALSE)%>%
     group_by(C_BioSense_ID)%>%
     slice(which.min(Recorded_Date_Time))
-  
+
 
   Time_Diff=LagTime%>%
-   mutate(Arrived=as.POSIXct(Arrived_Date_Time,format="%Y-%m-%d %H:%M:%S"),
-           Visit=as.POSIXct(C_Visit_Date_Time,format="%Y-%m-%d %H:%M:%S"),
-           Message=as.POSIXct(Message_Date_Time,format="%Y-%m-%d %H:%M:%S"),
-           Record=as.POSIXct(Recorded_Date_Time,format="%Y-%m-%d %H:%M:%S"),
+   transmute(C_Biosense_Facility_ID,
+          Arrived=as.POSIXct(Arrived_Date_Time,format="%Y-%m-%d %H:%M:%S"),
+          Visit=as.POSIXct(C_Visit_Date_Time,format="%Y-%m-%d %H:%M:%S"),
+          Message=as.POSIXct(Message_Date_Time,format="%Y-%m-%d %H:%M:%S"),
+          Record=as.POSIXct(Recorded_Date_Time,format="%Y-%m-%d %H:%M:%S"),
           lag_Record_Visit=as.numeric(difftime(Record,Visit,units="hours")),
          lag_Message_Record=as.numeric(difftime(Message,Record,units="hours")),
          lag_Arrival_Message=as.numeric(difftime(Arrived,Message,units="hours")),
