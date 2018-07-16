2d078f7dcdd073e5b2505d641db7873d0af38023
Mengjiao0714 <30472909+Mengjiao0714@users.noreply.github.com>
2018-07-16 15:38:55 -0500

    Create ethnicity_code_perc.R

---
+++ R/ethnicity_code_perc.R
@@ -0,0 +1,17 @@
+##Frequency of occurrence in data - what is actually occurring in the data and
+##how often each distinct entry is occurring and percentage of all visits each entry
+##accounts for. 
+
+## Ethnicity_Code
+ethnicity_code_perc<-function(data){
+  Ethnicity_Code=data%>%
+    select(C_BioSense_ID,Ethnicity_Code)%>%
+    distinct(C_BioSense_ID,.keep_all=TRUE)%>%
+    count(Ethnicity_Code)%>%
+    transmute(Ethnicity_Code,count=n,percentage=n/sum(n))
+  
+  return(
+    Ethnicity_Code
+  )
+  
+}
