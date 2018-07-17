9f921d118d7e3c9fee1f55472f2e91b41dd378cc
Mengjiao0714 <30472909+Mengjiao0714@users.noreply.github.com>
2018-07-17 08:08:23 -0500

    Create county_perc.R

---
+++ R/county_perc.R
@@ -0,0 +1,15 @@
+##Frequency of occurrence in data - what is actually occurring in the data and
+##how often each distinct entry is occurring and percentage of all visits each entry
+##accounts for.
+
+##county
+county_perc<-function(data){
+  County=data%>%
+    select(C_BioSense_ID, C_Patient_County)%>%
+    distinct(C_BioSense_ID,.keep_all=TRUE)%>%
+    count(C_Patient_County)%>%
+    transmute(C_Patient_County,count=n,percentage=n/sum(n))
+  return(
+    County
+  )
+}
