67e50c4233befaee87ee27f2e199675fc4a1ab7b
Mengjiao0714 <30472909+Mengjiao0714@users.noreply.github.com>
2018-07-17 11:38:27 -0500

    Create patient_class.R

---
+++ R/patient_class.R
@@ -0,0 +1,15 @@
+##Frequency of occurrence in data - what is actually occurring in the data and
+##how often each distinct entry is occurring and percentage of all visits each entry
+##accounts for. 
+## C_MFT_Patient_Class
+
+patient_class_perc<-function(data){
+    Patient_Class=data%>%
+    select(C_BioSense_ID, C_MFT_Patient_Class)%>%
+    distinct(C_BioSense_ID,.keep_all=TRUE)%>%
+    count(C_MFT_Patient_Class)%>%
+    transmute(C_MFT_Patient_Class,count=n,percentage=round(n/sum(n),3))
+  return(
+    Patient_Class
+  )
+}
