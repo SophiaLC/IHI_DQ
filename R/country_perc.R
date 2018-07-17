b64096df3acfd81fe67c5ead090567b431db86a5
Mengjiao0714 <30472909+Mengjiao0714@users.noreply.github.com>
2018-07-17 13:57:31 -0500

    Update country_perc.R

--- R/country_perc.R
+++ R/country_perc.R
@@ -6,7 +6,7 @@
 country_perc<-function(data){
   Country=data%>%
     select(C_BioSense_ID, Patient_Country)%>%
-    mutate(Patient_Country=case_when(is.na(Patient_Country)==TRUE~"NA"))%>%
+    mutate(Patient_Country=ifelse(is.na(Patient_Country),"NA","Patient_Country"))%>%
     distinct(C_BioSense_ID,.keep_all=TRUE)%>%
     count(Patient_Country)%>%
     transmute(Patient_Country,count=n,percentage=round(n/sum(n),3))
