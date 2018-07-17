849887169c9b8333d4917eeee03891adc7cf26b2
Mengjiao0714 <30472909+Mengjiao0714@users.noreply.github.com>
2018-07-17 12:56:46 -0500

    Update smoking_status_description_perc.R

--- R/smoking_status_description_perc.R
+++ R/smoking_status_description_perc.R
@@ -6,6 +6,7 @@
 smoking_status_description_perc<-function(data){
   Smoking_Status_Description=data%>%
     select(C_BioSense_ID, Smoking_Status_Description)%>%
+     mutate(Smoking_Status_Description=case_when(is.na(Smoking_Status_Description)==TRUE~"NA"))%>%
     distinct(C_BioSense_ID,.keep_all=TRUE)%>%
     count(Smoking_Status_Description)%>%
     transmute(Smoking_Status_Description,count=n,percentage=round(n/sum(n),3))
