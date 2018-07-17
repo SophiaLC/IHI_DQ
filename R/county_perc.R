1534382710944ca2d0c2864a138fdb950da3b017
Mengjiao0714 <30472909+Mengjiao0714@users.noreply.github.com>
2018-07-17 14:07:59 -0500

    Update county_perc.R

--- R/county_perc.R
+++ R/county_perc.R
@@ -6,7 +6,7 @@
 county_perc<-function(data){
   County=data%>%
     select(C_BioSense_ID, C_Patient_County)%>%
-    mutate(C_Patient_County=ifelse(is.na(C_Patient_County),"NA","C_Patient_County"))%>%
+    mutate(C_Patient_County=ifelse(is.na(C_Patient_County),"NA",C_Patient_County))%>%
     distinct(C_BioSense_ID,.keep_all=TRUE)%>%
     count(C_Patient_County)%>%
     transmute(C_Patient_County,count=n,percentage=round(n/sum(n),3))
