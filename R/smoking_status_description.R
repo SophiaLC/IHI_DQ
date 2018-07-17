b6eaccd4dbf50b8ff76863b8ed37a2544dddd77b
Mengjiao0714 <30472909+Mengjiao0714@users.noreply.github.com>
2018-07-17 12:01:52 -0500

    Create smoking_status_description.R

---
+++ R/smoking_status_description.R
@@ -0,0 +1,15 @@
+##Frequency of occurrence in data - what is actually occurring in the data and
+##how often each distinct entry is occurring and percentage of all visits each entry
+##accounts for.
+
+## smoking status description
+smoking_status_description_perc<-function(data){
+  Smoking_Status_Description=data%>%
+    select(C_BioSense_ID, Smoking_Status_Description)%>%
+    distinct(C_BioSense_ID,.keep_all=TRUE)%>%
+    count(Smoking_Status_Description)%>%
+    transmute(Smoking_Status_Description,count=n,percentage=round(n/sum(n),3))
+  return(
+    Smoking_Status_Description
+  )
+}
