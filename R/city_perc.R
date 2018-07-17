9d314b15251458f1872e7f0bad2b1f39edf500cc
Mengjiao0714 <30472909+Mengjiao0714@users.noreply.github.com>
2018-07-17 14:01:19 -0500

    Update city_perc.R

--- R/city_perc.R
+++ R/city_perc.R
@@ -6,7 +6,8 @@
 city_perc<-function(data){
   City=data%>%
     select(C_BioSense_ID, Patient_City)%>%
-     mutate(Patient_City=toupper(Patient_City),Patient_City=case_when(is.na(Patient_City)==TRUE~"NA"))%>%
+     mutate(Patient_City=toupper(Patient_City),
+            Patient_City=ifelse(is.na(Patient_City),"NA",Patient_City))%>%
     distinct(C_BioSense_ID,.keep_all=TRUE)%>%
     count(Patient_City)%>%
     transmute(Patient_City,count=n,percentage=round(n/sum(n),3))
