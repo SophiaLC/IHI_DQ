e95b7abb36293f2ffc67816bb7f5166603e64478
Mengjiao0714 <30472909+Mengjiao0714@users.noreply.github.com>
2018-07-17 11:33:36 -0500

    Create insurance_company_id.R

---
+++ R/insurance_company_id.R
@@ -0,0 +1,15 @@
+##Frequency of occurrence in data - what is actually occurring in the data and
+##how often each distinct entry is occurring and percentage of all visits each entry
+##accounts for. 
+
+## Insurance_Company_ID
+insurance_company_id_perc<-function(data){
+  Insurance_Company_ID=data%>%
+    select(C_BioSense_ID, Insurance_Company_ID)%>%
+    distinct(C_BioSense_ID,.keep_all=TRUE)%>%
+    count(Insurance_Company_ID)%>%
+    transmute(Insurance_Company_ID,count=n,percentage=round(n/sum(n),3))
+  return(
+    Insurance_Company_ID
+  )
+}
