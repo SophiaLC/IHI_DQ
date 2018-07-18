7df43b95f7753929cb95318087dccb9da4aa9ca1
Mengjiao0714 <30472909+Mengjiao0714@users.noreply.github.com>
2018-07-18 15:30:20 -0500

    Update city_perc.R

--- R/city_perc.R
+++ R/city_perc.R
@@ -10,7 +10,7 @@ city_perc<-function(data){
             Patient_City=ifelse(is.na(Patient_City),"NA",Patient_City))%>%
     distinct(C_BioSense_ID,.keep_all=TRUE)%>%
     count(Patient_City)%>%
-    transmute(Patient_City,count=n,percentage=round(n/sum(n),3))
+    transmute(Patient_City,count=n,percentage=round(100*n/sum(n),2))
   return(
     City
   )
