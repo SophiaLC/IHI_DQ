f5b0f4f8f8d55985686474e7fcd1ee8b922ff8ec
Mengjiao0714 <30472909+Mengjiao0714@users.noreply.github.com>
2018-07-19 11:28:50 -0500

    Update write_reports.R

--- R/write_reports.R
+++ R/write_reports.R
@@ -62,7 +62,7 @@ write_reports <- function(username, password, table, mft,raw, start, end, direct
   sheet1 <- addWorksheet(wb, "Required Nulls")
   # putting statewide above the filter
   writeData(wb, sheet1, statewides$statewide_reqnull, 
-            startCol=2, startRow=1, colNames=FALSE)
+            startCol=4, startRow=1, colNames=FALSE)
   # writing data table below
   writeDataTable(wb, sheet1,
                  state_req_nulls %>% 
@@ -77,7 +77,7 @@ write_reports <- function(username, password, table, mft,raw, start, end, direct
   sheet2 <- addWorksheet(wb, "Optional Nulls")
   # putting statewide above the filter
  writeData(wb, sheet2, statewides$statewide_optnull, 
-            startCol=2, startRow=1, colNames=FALSE)
+            startCol=4, startRow=1, colNames=FALSE)
   # writing data table below
   writeDataTable(wb, sheet2,
                  state_opt_nulls %>% 
@@ -92,7 +92,7 @@ write_reports <- function(username, password, table, mft,raw, start, end, direct
   sheet3 <- addWorksheet(wb, "Invalids")
   # putting statewide above the filter
   writeData(wb, sheet3, statewides$statewide_invalids, 
-            startCol=2, startRow=1, colNames=FALSE)
+            startCol=4, startRow=1, colNames=FALSE)
   # writing data table below
   writeDataTable(wb, sheet3,
                  state_invalids %>% 
@@ -207,7 +207,7 @@ write_reports <- function(username, password, table, mft,raw, start, end, direct
       filter(Measure=="Percent") %>% # only percent
       select(-Location, -Measure) %>% # select vars only needed
       gather(Field, State_Percent, 1:ncol(.)) %>% # put into long format
-      left_join(one_facility_summary(state_req_nulls, i), ., by="Field") # join with one facility summary
+      left_join(one_facility_summary(state_req_nulls[,-c(2,3)], i), ., by="Field") # join with one facility summary
     writeDataTable(wb, sheet2, facsheet2data, firstColumn=TRUE, bandedRows=TRUE) # write to table
     setColWidths(wb, sheet2, 1:ncol(facsheet2data), "auto") # format sheet
     freezePane(wb, sheet2, firstActiveRow=2) # format sheet
@@ -218,7 +218,7 @@ write_reports <- function(username, password, table, mft,raw, start, end, direct
       filter(Measure=="Percent") %>% # only percent
       select(-Location, -Measure) %>% # select vars only needed
       gather(Field, State_Percent, 1:ncol(.)) %>% # put into long format
-      left_join(one_facility_summary(state_opt_nulls, i), ., by="Field") # join with one facility summary
+      left_join(one_facility_summary(state_opt_nulls[,-c(2,3)], i), ., by="Field") # join with one facility summary
     writeDataTable(wb, sheet3, facsheet3data, firstColumn=TRUE, bandedRows=TRUE) # write to table
     setColWidths(wb, sheet3, 1:ncol(facsheet3data), "auto") # format sheet
     freezePane(wb, sheet3, firstActiveRow=2) # format sheet
@@ -229,7 +229,7 @@ write_reports <- function(username, password, table, mft,raw, start, end, direct
       filter(Measure=="Percent") %>% # only percent
       select(-Location, -Measure) %>% # select vars only needed
       gather(Field, State_Percent, 1:ncol(.)) %>% # put into long format
-      left_join(one_facility_summary(state_invalids, i), ., by="Field") # join with one facility summary
+      left_join(one_facility_summary(state_invalids[,-c(2,3)], i), ., by="Field") # join with one facility summary
     writeDataTable(wb, sheet4, facsheet4data, firstColumn=TRUE, bandedRows=TRUE) # write to table
     setColWidths(wb, sheet4, 1:ncol(facsheet4data), "auto") # format sheet
     freezePane(wb, sheet4, firstActiveRow=2) # format sheet
