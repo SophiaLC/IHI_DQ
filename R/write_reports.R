269a020e9d2b040144549e56bb7af6330c2d0681
Mengjiao0714 <30472909+Mengjiao0714@users.noreply.github.com>
2018-07-20 11:28:18 -0500

    Update write_reports.R

--- R/write_reports.R
+++ R/write_reports.R
@@ -248,7 +248,9 @@ write_reports <- function(username, password, table, mft,raw, start, end, direct
     distinct()
     
     ## compute the # of batches per day for each Feed_Name/facility
-    Batch_Info=batch_info(batchdata)%>%
+    Batch_Info=batch_info(batchdata)
+    
+    Batch_Info=Batch_Info%>%
                filter(Feed_Name==Batch_Mean$Feed_Name)
     
     writeDataTable(wb, sheet5, Batch_Mean, firstColumn=TRUE, bandedRows=TRUE)
