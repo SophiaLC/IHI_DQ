345c8a8e42086013b6b7e18e24015285b2961229
Mengjiao0714 <30472909+Mengjiao0714@users.noreply.github.com>
2018-07-25 12:10:09 -0500

    Update trigger_event_perc.R

--- R/trigger_event_perc.R
+++ R/trigger_event_perc.R
@@ -2,13 +2,13 @@
 ##how often each distinct entry is occurring and percentage of all visits each entry
 ##accounts for.
 ##Trigger_Event
-
+##number_visits=n_groups(group_by(data,C_BioSense_ID)), this number of visits will be computed in write_report function
 trigger_event_perc<-function(data){
   Trigger_Event=data%>%
     select(C_BioSense_ID, Trigger_Event)%>%
     distinct(C_BioSense_ID,Trigger_Event,.keep_all=TRUE)%>%
     count(Trigger_Event)%>%
-    transmute(Trigger_Event,count=n,percentage=round(100*n/sum(n),2))
+    transmute(Trigger_Event,count=n,percentage=round(100*n/number_visits,2))
   return(
     Trigger_Event
   )
