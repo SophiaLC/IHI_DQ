08adb09ed2311df0bed6c9f60571f7b5bc6ac5ee
Mengjiao0714 <30472909+Mengjiao0714@users.noreply.github.com>
2018-07-25 10:53:17 -0500

    Update patient_class_perc.R

--- R/patient_class_perc.R
+++ R/patient_class_perc.R
@@ -4,10 +4,10 @@
 ## C_MFT_Patient_Class
 
 patient_class_perc<-function(data){
-    Patient_Class=data%>%
+  Patient_Class=data%>%
     select(C_BioSense_ID, Patient_Class_Code)%>%
     mutate(Patient_Class_Code=ifelse(is.na(Patient_Class_Code),"NA",Patient_Class_Code))%>%
-    distinct(C_BioSense_ID,.keep_all=TRUE)%>%
+    distinct(C_BioSense_ID,Patient_Class_Code,.keep_all=TRUE)%>%
     count(Patient_Class_Code)%>%
     transmute(Patient_Class_Code,count=n,percentage=round(100*n/sum(n),2))
   return(
