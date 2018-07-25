d2f2e38512fe6a884f671c3c42a8bc65b4677999
Mengjiao0714 <30472909+Mengjiao0714@users.noreply.github.com>
2018-07-25 15:53:50 -0500

    Update diagnosis_type_perc.R

--- R/diagnosis_type_perc.R
+++ R/diagnosis_type_perc.R
@@ -5,13 +5,8 @@
 ## Diagnosis_Type
 
 diagnosis_type_perc<-function(data){
-  Diagnosis_Type=data%>%
-    select(C_BioSense_ID, Diagnosis_Type)%>%
-    mutate(Diagnosis_Type=ifelse(is.na(Diagnosis_Type),"NA",Diagnosis_Type))%>%
-    distinct(C_BioSense_ID,.keep_all=TRUE)%>%
-    count(Diagnosis_Type)%>%
-    transmute(Diagnosis_Type,count=n,percentage=round(100*n/sum(n),2))
-  return(
-    Diagnosis_Type
-  )
+  diagnosis=unlist(strsplit(paste0(data$Diagnosis_Type),";"))
+  Diagnosis_Type=diagnosis[diagnosis!=""]
+  as.data.frame(table(Diagnosis_Type))%>%
+                                  arrange(desc(Freq))
 }
