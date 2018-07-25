e995f019fc6e564b83a01b2bb1b89be4aa43e685
Mengjiao0714 <30472909+Mengjiao0714@users.noreply.github.com>
2018-07-25 11:35:37 -0500

    Update trigger_event_perc.R

--- R/trigger_event_perc.R
+++ R/trigger_event_perc.R
@@ -6,6 +6,7 @@
 trigger_event_perc<-function(data){
   Trigger_Event=data%>%
     select(C_BioSense_ID, Trigger_Event)%>%
+    distinct(C_BioSense_ID,Trigger_Event,.keep_all=TRUE)%>%
     count(Trigger_Event)%>%
     transmute(Trigger_Event,count=n,percentage=round(100*n/sum(n),2))
   return(
