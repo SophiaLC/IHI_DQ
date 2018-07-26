682a3f2f79ff27b4fff39a6162d4e5a4533996aa
Mengjiao0714 <30472909+Mengjiao0714@users.noreply.github.com>
2018-07-26 09:10:28 -0500

    Update race_description_perc.R

--- R/race_description_perc.R
+++ R/race_description_perc.R
@@ -7,7 +7,7 @@ race_description_perc<-function(data){
   Race_Description=data%>%
     select(C_BioSense_ID,Race_Description)%>%
     mutate(Race_Description=toupper(Race_Description),
-           Race_Description=ifelse(is.na(Race_Description),"NA",Race_Description))%>%
+           Race_Description=ifelse(is.na(Race_Description),"NULL",Race_Description))%>%
     distinct(C_BioSense_ID,.keep_all=TRUE)%>%
     count(Race_Description)%>%
     transmute(Race_Description,count=n,percentage=round(100*n/sum(n),2))
