20dbf6a1b55de99c63d300bbc487844f74ba2672
Mengjiao0714 <30472909+Mengjiao0714@users.noreply.github.com>
2018-07-26 09:06:56 -0500

    Update discharge_disposition_perc.R

--- R/discharge_disposition_perc.R
+++ R/discharge_disposition_perc.R
@@ -6,8 +6,8 @@
 discharge_disposition_perc<-function(data){
   Discharge_Disposition=data%>%
     select(C_BioSense_ID, Discharge_Disposition)%>%
-    mutate(Discharge_Disposition=ifelse(is.na(Discharge_Disposition),"NA",Discharge_Disposition))%>%
-    distinct(C_BioSense_ID,.keep_all=TRUE)%>%
+    mutate(Discharge_Disposition=ifelse(is.na(Discharge_Disposition),"NULL",Discharge_Disposition))%>%
+    distinct(C_BioSense_ID,Discharge_Disposition,.keep_all=TRUE)%>%
     count(Discharge_Disposition)%>%
     transmute(Discharge_Disposition,count=n,percentage=round(100*n/sum(n),2))
   return(
