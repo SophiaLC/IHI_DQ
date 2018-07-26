00fabc4267af82d63a2c5ae4c099169c50d24cf9
Mengjiao0714 <30472909+Mengjiao0714@users.noreply.github.com>
2018-07-26 09:07:56 -0500

    Update facility_type_code_perc.R

--- R/facility_type_code_perc.R
+++ R/facility_type_code_perc.R
@@ -6,7 +6,7 @@
 facility_type_code_perc<-function(data){
   Facility_Type_Code=data%>%
     select(C_BioSense_ID, Facility_Type_Code)%>%
-    mutate(Facility_Type_Code=ifelse(is.na(Facility_Type_Code),"NA",Facility_Type_Code))%>%
+    mutate(Facility_Type_Code=ifelse(is.na(Facility_Type_Code),"NULL",Facility_Type_Code))%>%
     distinct(C_BioSense_ID,.keep_all=TRUE)%>%
     count(Facility_Type_Code)%>%
     transmute(Facility_Type_Code,count=n,percentage=round(100*n/sum(n),2))
