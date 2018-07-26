8829fabce4d9b9c7aaccb4274f98b6fa434ff73f
Mengjiao0714 <30472909+Mengjiao0714@users.noreply.github.com>
2018-07-26 09:10:12 -0500

    Update race_code_perc.R

--- R/race_code_perc.R
+++ R/race_code_perc.R
@@ -6,7 +6,7 @@
 race_code_perc<-function(data){
   Race_Code=data%>%
     select(C_BioSense_ID,Race_Code)%>%
-    mutate(Race_Code=ifelse(is.na(Race_Code),"NA",Race_Code))%>%
+    mutate(Race_Code=ifelse(is.na(Race_Code),"NULL",Race_Code))%>%
     distinct(C_BioSense_ID,.keep_all=TRUE)%>%
     count(Race_Code)%>%
     transmute(Race_Code,count=n,percentage=round(100*n/sum(n),2))
