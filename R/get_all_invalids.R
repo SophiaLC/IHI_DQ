608ab6eff30adea0220b64a955a88f9671f9cd32
Sophia Crossen <30636396+SophiaLC@users.noreply.github.com>
2018-07-31 14:39:55 -0500

    Update get_all_invalids.R

--- R/get_all_invalids.R
+++ R/get_all_invalids.R
@@ -15,7 +15,7 @@ get_all_invalids <- function(data) {
   # make summaries into a list
   invalid_summaries <- list(admit_source_invalid(data)[[2]], age_invalid(data)[[2]], any_e_invalid(data)[[2]],
                             blood_pressure_invalid(data)[[2]], cc_ar_invalid(data)[[2]], country_invalid(data)[[2]],
-                            county_invalid(data)[[2]], death_invalid(data)[[2]], diagnosis_type_invalid(data)[[2]], 
+                            death_invalid(data)[[2]], diagnosis_type_invalid(data)[[2]], 
                             discharge_disposition_invalid(data)[[2]], ethnicity_invalid(data)[[2]], 
                             facility_type_invalid(data)[[2]], fpid_mrn_invalid(data)[[2]],
                             gender_invalid(data)[[2]], height_invalid(data)[[2]], patient_class_invalid(data)[[2]],
