cba30331fa305f495bbdca88920dc8091f6e561b
Sophia Crossen <30636396+SophiaLC@users.noreply.github.com>
2020-10-15 14:24:01 -0400

    Update cc_ar_invalid.R
    Update to IHI vars

--- R/cc_ar_invalid.R
+++ R/cc_ar_invalid.R
@@ -13,7 +13,7 @@ cc_ar_invalid <- function(data) {
 
   # generate examples
   cc_ar_examples <- data %>% # take data
-    select(c(C_BioSense_ID, C_Biosense_Facility_ID, Chief_Complaint_Text, Admit_Reason_Description)) %>% # get vars you need
+    select(c(C_Visit_ID, C_Facility_ID, Chief_Complaint_Text, Admit_Reason_Description)) %>% # get vars you need
     mutate(
       Chief_Complaint_Text=toupper(Chief_Complaint_Text), # upper everything
       Admit_Reason_Description=toupper(Admit_Reason_Description), # upper everything
@@ -23,7 +23,7 @@ cc_ar_invalid <- function(data) {
 
   # generate summary
   cc_ar_summary <- cc_ar_examples %>% # take examples
-    group_by(C_BioSense_ID) %>% # group by patient visit
+    group_by(C_Visit_ID) %>% # group by patient visit
     mutate(
       Any_Chief_Complaint_Text_Short=case_when(
         all(is.na(Chief_Complaint_Text_Short)) ~ NA,
@@ -38,7 +38,7 @@ cc_ar_invalid <- function(data) {
     ) %>%
     slice(1) %>% # one obv per visit
     ungroup() %>% # explicitly ungroup
-    group_by(C_Biosense_Facility_ID) %>% # group by facility
+    group_by(C_Facility_ID) %>% # group by facility
     summarise(Chief_Complaint_Text_Short.Percent=round(mean(Any_Chief_Complaint_Text_Short, na.rm=TRUE)*100,2),
               Admit_Reason_Description_Short.Percent=round(mean(Any_Admit_Reason_Description_Short, na.rm=TRUE)*100,2),
               Chief_Complaint_Text_Short.Count=sum(Any_Chief_Complaint_Text_Short, na.rm=TRUE),
