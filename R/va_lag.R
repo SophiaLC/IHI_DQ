f66db516626d7a0db886a664d013e9bc2191207a
Sophia Crossen <30636396+SophiaLC@users.noreply.github.com>
2020-10-15 18:01:49 -0400

    Update va_lag.R

--- R/va_lag.R
+++ R/va_lag.R
@@ -1,11 +1,11 @@
 #' Visit-Arrival Lag
 #'
 #' @description 
-#' Calculates the average time lag between when: a patient arrives and the first record for their visit is received by the NSSP BioSense Platform.
+#' Calculates the average time lag between when: a patient arrives and the first record for their visit is received by the Platform.
 #' A summary is returned at the facility level.
 #' 
-#' @param data The raw data from BioSense on which you will do these summaries.
-#' @param names A table of Facility_Name and C_Biosense_Facility_ID which taken from the MFT using the `pull_data` function.
+#' @param data The raw data on which you will do these summaries.
+#' @param names A table of Facility_Name and C_Facility_ID which taken from the MFT using the `pull_data` function.
 #' @param offset The number of hours you wish to offset Arrived_Date_Time (which is in UTC). The offset value is how far off your local time zone is from UTC. 
 #' For example, the Central Time Zone would set this to 5 or 6, depending on if it is daylight savings or not. This value should be an integer. 
 #' @return A data frame with three columns: facility name, facility ID, and average lag between patient visit and first record arrival.
@@ -26,6 +26,6 @@ va_lag <- function(data, names, offset) {
       ungroup() %>% # ungroup explicitly
       group_by(C_Facility_ID) %>% # regroup by facility
       summarise(First_Message=round(mean(lag, na.rm=TRUE),2)) %>% # get the average, round by two digits
-      right_join(names, ., by="C_Biosense_Facility_ID") # join with the names data
+      right_join(names, ., by="C_Facility_ID") # join with the names data
   )
 }
