f122b0d3ff4affa19373ab26a8a33098dd179c6b
Sophia Crossen <30636396+SophiaLC@users.noreply.github.com>
2020-10-15 17:47:21 -0400

    Update diagnosis_type_invalid.R

--- R/diagnosis_type_invalid.R
+++ R/diagnosis_type_invalid.R
@@ -10,7 +10,7 @@
 #' 
 #' You can view the concept codes that are considered valid by calling `data("diagnosis_type")`.
 #' 
-#' @param data The raw data from BioSense on which you will do the invalid Diagnosis_Type checks.
+#' @param data The raw data on which you will do the invalid Diagnosis_Type checks.
 #' @return A list of two data frames: examples and summary for invalid Diagnosis_Type.
 #' @import dplyr
 #' @export
