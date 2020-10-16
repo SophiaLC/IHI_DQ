f45c5d11ff6a707a2953bc6143c8cbb18ea80a39
Sophia Crossen <30636396+SophiaLC@users.noreply.github.com>
2020-10-16 11:29:05 -0400

    Update va_lag.R

--- R/va_lag.R
+++ R/va_lag.R
@@ -6,7 +6,7 @@
 #' 
 #' @param data The raw data on which you will do these summaries.
 #' @param names A table of Facility_Name and C_Facility_ID which taken from the MFT using the `pull_data` function.
-#' @param offset The number of hours you wish to offset Arrived_Date_Time (which is in UTC). The offset value is how far off your local time zone is from UTC. 
+#' @param offset The number of hours you wish to offset Date_Entry_Added (which is in UTC). The offset value is how far off your local time zone is from UTC. 
 #' For example, the Central Time Zone would set this to 5 or 6, depending on if it is daylight savings or not. This value should be an integer. 
 #' @return A data frame with three columns: facility name, facility ID, and average lag between patient visit and first record arrival.
 #' @import dplyr
@@ -15,13 +15,13 @@
 va_lag <- function(data, names, offset) {
   return(
     data %>% # take raw data
-      select(C_Visit_ID, C_Facility_ID, Arrived_Date_Time, C_Visit_Date_Time) %>% # select only vars we need
+      select(C_Visit_ID, C_Facility_ID, Date_Entry_Added, C_Visit_Date_Time) %>% # select only vars we need
       group_by(C_Visit_ID) %>% # group by patient visit
-      slice(which.min(Arrived_Date_Time)) %>% # get the first arrived date time
+      slice(which.min(Date_Entry_Added)) %>% # get the first arrived date time
       slice(1) %>% # get one, just in case there are ties
       mutate(
-        Arrived_Date_Time = lubridate::ymd_hms(Arrived_Date_Time) - lubridate::hours(offset), # adjust utc arrived to local time
-        lag=as.numeric(difftime(Arrived_Date_Time, C_Visit_Date_Time, units="hours")) # get lag, in hours
+        Date_Entry_Added = lubridate::ymd_hms(Date_Entry_Added) - lubridate::hours(offset), # adjust utc arrived to local time
+        lag=as.numeric(difftime(Date_Entry_Added, C_Visit_Date_Time, units="hours")) # get lag, in hours
       ) %>% 
       ungroup() %>% # ungroup explicitly
       group_by(C_Facility_ID) %>% # regroup by facility
